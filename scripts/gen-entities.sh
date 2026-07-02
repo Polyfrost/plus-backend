16eeeeccdf8d3d2278e0814eeec2c7c4adf4cc0a
Yanice <git@yanice.me>
2026-07-02 17:00:29 +0200

    fix(script): no explicit dir on gen db entities

--- scripts/gen-entities.sh
+++ scripts/gen-entities.sh
@@ -1,6 +1,6 @@
 set -euxo pipefail
 
-sea-orm-cli migrate up
+sea-orm-cli migrate up -d "./database/migrations"
 rm -r ./database/entities/src/entities
 sea-orm-cli generate entity -o ./database/entities/src/entities \
     --enum-extra-derives schemars::JsonSchema,serde::Deserialize,serde::Serialize,Hash \
